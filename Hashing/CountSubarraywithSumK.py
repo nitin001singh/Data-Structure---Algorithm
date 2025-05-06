b6f7eef68053e04344352292dbdb6cb773332a4c
Nitin Singh <nitin001.singh@gmail.com>
2025-05-06 19:04:12 +0530

    Find count of subarray of sum k

--- Hashing/CountSubarraywithSumK.py
+++ Hashing/CountSubarraywithSumK.py
@@ -1,48 +1,55 @@
+# Generate all Subarray
+
+# def generate_all_subarrays(nums):
+#     n = len(nums)
+#     subarrays = []
+#     for i in range(n): 
+#         for j in range(i, n): 
+#             subarrays.append(nums[i:j+1])  
+#     return subarrays
+
+# nums = [1, 7, 5, 9]
+# all_subarrays = generate_all_subarrays(nums)
+# print(all_subarrays)
+
+
+
 # Brute Force 
 # Tc : O(N^2)
 # SC : O(1)
 
-# class Solution:
-#     def answer(self, nums,k):
-#         n = len(nums)
-#         count = 1
-#         for i in range(n):
-#             currentSum = 0
-#             for j in range(i, n):
-#                 currentSum += nums[j]
-#                 if currentSum == k:
-#                     count += 1
-                    
-                
-#         return count
-            
-    
-# result = Solution().answer([1, 7, 5, 9, 2, 12, 3], 9)
-# print(result)
-
-
-
 class Solution:
-    def answer(self, nums, k):
-        count = 0
+    def answer(self, nums,k):
         n = len(nums)
-        prefix_sum = [0] * n
-        prefix_sum[0] = nums[0]
-        
-        for i in range(1, n):
-            prefix_sum[i] = prefix_sum[i - 1] + nums[i]
-        
+        count = 0
         for i in range(n):
+            currentSum = 0
             for j in range(i, n):
-                subarray_sum = prefix_sum[j] - (prefix_sum[i - 1] if i > 0 else 0)
-                # print(subarray_sum)
-                if subarray_sum == k:
+                currentSum += nums[j]
+                if currentSum == k:
                     count += 1
-        
-        return count
 
+        return count
+    
 result = Solution().answer([1, 7, 5, 9, 2, 12, 3], 9)
 print(result)
 
 
+class Solution:
+    def answer(self, nums, k):
+        summ = 0
+        count = 0
+        prefix_sum_count = {0: 1}
+
+        for num in nums:
+            summ += num
+            if (summ - k) in prefix_sum_count:
+                count += prefix_sum_count[summ - k]
+            prefix_sum_count[summ] = prefix_sum_count.get(summ, 0) + 1
+
+        return count
+
+
+result = Solution().answer([1, 7, 5, 9, 2, 12, 3], 9)
+print(result)
 
