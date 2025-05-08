1853831ca3330cf5dbf3ca0ab2ceabbcf113a2a5
Nitin Singh <nitin001.singh@gmail.com>
2025-05-08 10:12:02 +0530

    DSA Programmes

---
+++ Hashing/findCommonChar.py
@@ -0,0 +1,20 @@
+class Solution:
+    def commonChars(self, words):
+        min_freq = [100] * 26 
+        
+        for word in words:
+            freq = [0] * 26
+            for char in word:
+                freq[ord(char) - ord('a')] += 1
+
+            for i in range(26):
+                min_freq[i] = min(min_freq[i], freq[i])
+
+        result = []
+        for i in range(26):
+            result.extend(   [chr(i + ord('a'))]   *   min_freq[i]   )
+
+        return result
+
+result = Solution().commonChars(["bella", "label", "roller"])
+print(result)
