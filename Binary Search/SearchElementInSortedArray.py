2f9293aa1182d07a795c07c7aeb3e4d385ead4ca
Nitin Singh <nitin001.singh@gmail.com>
2025-05-09 15:37:26 +0530

    DSA Programmes

--- Binary Search/SearchElementInSortedArray.py
+++ Binary Search/SearchElementInSortedArray.py
@@ -7,13 +7,15 @@ class Solution:
         while l <= r:
             mid = (l+r) // 2
             if nums[mid] == k:
-                return mid
+                return True
             
             elif nums[mid] > k:
                 r = mid - 1
                 
             else:
                 l = mid + 1
+                
+        return False
         
-result = Solution().answer([1,5,2,3,4,9,7,2,8,5], 9)
+result = Solution().answer([1,5,2,3,4,9,7,2,8,5], 7)
 print(result)
