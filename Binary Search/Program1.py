c3c6d0f67012f0238b17f4420e3c1f62cb425f98
Nitin Singh <nitin001.singh@gmail.com>
2025-05-09 16:32:30 +0530

    DSA Programmes

--- Binary Search/Program1.py
+++ Binary Search/Program1.py
@@ -2,21 +2,61 @@
 
 # TC : O(NlogN)
 # SC : O(1)
+# class Solution:
+#     def answer(self, nums, target):
+#         nums.sort()
+#         opt = [-1,-1]
+#         for x in range(len(nums)):
+#             if nums[x] == target:
+#                 opt[0] = x
+#                 break
+            
+#         for y in range(len(nums)-1, -1 ,-1):
+#             if nums[y] == target:
+#                 opt[1] = y
+#                 break
+            
+#         return opt       
+        
+# result = Solution().answer([1,2,3,4,5,4,6,8,1,4], 4)
+# print(result)
+
+
+
+
+
+# TC : O(NlogN)
+# SC : O(1)
 class Solution:
     def answer(self, nums, target):
         nums.sort()
         opt = [-1,-1]
-        for x in range(len(nums)):
-            if nums[x] == target:
-                opt[0] = x
-                break
+        l = 0
+        r = len(nums) -1
+        while l <= r:
+            mid = (l+r) // 2
+            if nums[mid] == target:
+                opt[0] = mid
+                r = mid -1
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
             
-        for y in range(len(nums)-1, -1 ,-1):
-            if nums[y] == target:
-                opt[1] = y
-                break
             
+        l = 0
+        r = len(nums) -1
+        while l <= r:
+            mid = (l+r) // 2
+            if nums[mid] == target:
+                opt[1] = mid
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1       
+
         return opt       
         
-result = Solution().answer([1,2,3,4,5,4,6,8,1,4], 4)
+result = Solution().answer([5,7,7,8,8,10], 8)
 print(result)
