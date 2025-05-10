7f52c0c179a98f21f5cda53a737eec6c595df2b5
Nitin Singh <nitin001.singh@gmail.com>
2025-05-10 17:08:28 +0530

    DSA Programmes

---
+++ Greedy/Program1.py
@@ -0,0 +1,16 @@
+# TC : O(NlogN)
+# SC : O(1)
+
+class Solution:
+    def answer(self, num1, num2):
+        num1.sort()
+        num2.sort()
+        
+        mul = 0
+        for i in range(len(num1)):
+            mul += (num1[i] * num2[i])
+            
+        return mul, num1, num2
+    
+result = Solution().answer([1, 3, -5], [-2, 4, 1])
+print(result) 
---
+++ Greedy/Program2.py
@@ -0,0 +1,22 @@
+# TC : O(NlogN)
+# SC : O(1)
+
+class Solution:
+    def answer(self, num1, num2):
+        num1.sort()
+        num2.sort(reverse=True)
+        
+        mul = 0
+        for i in range(len(num1)):
+            mul += (num1[i] * num2[i])
+            
+        return mul
+    
+result = Solution().answer([1, 3, -5], [-2, 4, 1])
+print(result) 
+
+
+1    3   -5
+-2   1   4
+
+-2  3  -20         
