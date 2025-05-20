02b0d95e345c769cac5b5204ba5826058cb0cfbb
Nitin Singh <nitin001.singh@gmail.com>
2025-05-20 17:23:18 +0530

    DSA Programmes

--- Hashing/MicrosoftOA.py
+++ Hashing/MicrosoftOA.py
@@ -1,6 +1,6 @@
 # We are given an array of size N ; we are told to pick up any 2 numbers from the array such that their digit sum is equal ; if their digit sum is equal ; calculate their sum ; question is saying to find the maximum possible sum 
 
-
+# BruteForce
 # TC : O(N^2)
 # SC: O(1)
 
@@ -20,8 +20,43 @@ class Solution:
                 
         return ans            
             
+             
+    def digiSum(self, num):    # TC: O(log10(num))
+        summ = 0
+        while num > 0:
+            summ += num % 10
+            num //= 10
             
-    def digiSum(self, num):
+        return summ
+        
+
+result = Solution().answer([51 ,71 ,17 ,42] )
+print(result)
+
+
+
+
+# Optimal
+# TC : O(N)
+# SC : O(N)
+
+class Solution:
+    def answer(self, nums):
+        hashmap = {}
+        maxi = 0
+        for val in nums:
+            digi_sum = self.digiSumMap(val)
+            if digi_sum in hashmap:
+                summ = hashmap[digi_sum] + val
+                maxi = max(maxi, summ)
+                    
+            else:
+                hashmap[digi_sum] = val
+                
+        return  maxi           
+
+             
+    def digiSumMap(self, num):    # TC: O(log10(num))
         summ = 0
         while num > 0:
             summ += num % 10
