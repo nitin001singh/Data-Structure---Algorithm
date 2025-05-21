9506dff670396ea17492dd00c498175ddd213a18
Nitin Singh <nitin001.singh@gmail.com>
2025-05-21 20:14:12 +0530

    DSA Programmes

--- OA/Program7.py
+++ OA/Program7.py
@@ -1,21 +1,28 @@
-
 class Solution:
-    def answer(self, lamps, points):
+    def answer(self, nums, k):
+        summ = 0
+        length = float("inf")
+        count = 0
+        n = len(nums)
+        hashmap = {0: -1}
 
-        size = 100000    #size: 10^5
-        lamp = [0] * size
+        for i in range(n):
+            summ += nums[i]
+
+            if summ - k in hashmap:
+                curr_len = i - hashmap[summ - k]
+                if curr_len < length:
+                    length = curr_len
+                    count = 1
+                elif curr_len == length:
+                    count += 1
+
+
+            if summ not in hashmap:
+                hashmap[summ] = i
+
+        return count if length != float("inf") else 0
+
         
-        for l , r in lamps:
-            lamp[l] += 1
-            lamp[r+1] -= 1
-
-        for x in range(1, len(lamp)):
-            lamp[x] = lamp[x-1] + lamp[x]
-
-
-        for p in points:
-            print(lamp[p], end=" ")
-
-
-result = Solution().answer( [[1,7],[5,11],[7,9]], [7,1,5,10,9,15] )
-# print(result)
+result = Solution().answer( [10,5,2,7,1,9,8,7], 15)
+print(result)
